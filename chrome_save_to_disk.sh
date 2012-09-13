792515ced6166f89d189fb52fc8941da21abc0ac
Cedric Porter <cedricporter@gmail.com>
2012-09-13 15:32:22 +0800

    change the order of echo, so that we can see its output when start

--- chrome_save_to_disk.sh
+++ chrome_save_to_disk.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-echo "save from shm to disk"
-cp -rf /dev/shm/cedricporter /home/cedricporter/shm_backup
-
 file=/home/cedricporter/shm_backup/save.log
+
+echo "save from shm to disk" 
+
+notify-send "Start Chrome saving procedure!"
 echo "----- Start Saving -----" >> $file
+cp -rf /dev/shm/cedricporter /home/cedricporter/shm_backup 
 date >> $file
 echo "----- End -----" >> $file
+
 notify-send "Chrome saving to disk is done!"
 
 
